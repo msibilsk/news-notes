6312bbbc0a16b470ea7f8d939c2329b6ce006a58
Mary Willis <msibilsk@gmail.com>
2017-03-27 13:50:54 -0500

    got a message to render if there are no saved articles, but still have not solved the mystery of the scraped articles not rendering

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -33,10 +33,11 @@ router.get("/", function(req, res) {
     Article.find({}, function(error, found) {
         if (error) {
             console.log(error);
-            res.render("index");
+        }else if (found.length === 0) {
+            res.render("empty")
         } else {
             var hbsObject = {
-                articles: found
+              articles: found
             };
             res.render("index", hbsObject);
         }
